b1e7a77ce6613fac65434f73fc85a518190de649
Naveen Gande <ngandeus.ibm.com@NGMAC.local>
2020-03-05 12:36:46 -0600

    Check MD file

--- demo/getovminfo.py
+++ demo/getovminfo.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env /usr/bin/python
-#
-##############################################
+# Created by Naveen Gande for Geico(ngande@us.ibm.com)
+# This script will connect to OVM and provide  following Information
+# If you don't Specify the username - it will assume default user as admin and ask for password
+# Actions:
+#       listvms     - It will list all VMs on the OVM with corresponding Server where it is hosted
+#       listsrvs    - It will list all OVS servers
+#       showvmd     - List of all disk attached to particular VM
+#       srvdisks    - List disks attached to OVS server
+#       adddisks    - It will take your answer file and  will create all commands which you need to run on OVM
+#                       - adddisks action will do following checks
+#                       - It will validate your answerfile by checking following
+#                               -  Check any duplication slot numbers in your file
+#                               -  Check any duplication on provided disks
+#                               -  Check any duplicates in the disk names
+#                               -  It will validate all slot numbers - whether it is used already on the VM
+#                               -  Will check if the disknames are already used on the VM
+#                               -  Will check if the physical disks are already attached to VM
+# Answer File Specification:
+#    - It should be comma sperated
+#    - one disk info per one line
+#    - Header Needs to be added
+# Sample  answer file:
+# slot,disk,sharble,diskname
+# 12,6005076810810103F000000000000056,NO,a0680o3odbsd415IBM-OS
+# 13,6005076810810103F000000000000057,nO,a0680o3odbsd415IBM-swap
+# 14,6005076810810103F00000000000006D,YES,+ocr_vote1-a0680o3odbsd415_416_417
+###################################################################################################################
 
 import argparse
 import sys
@@ -401,13 +426,16 @@ def main():
                                     )
 
                     f.close()
-                    with open(fname_sorted, "w") as f1:
-                        with open(fname, "r") as f:
-                            data = f.readlines()
-                            data.sort()
-                            for line in data:
-                                f.write(line)
-                    f1.close()
+
+                    with open(fname, "r") as f:
+                        data = f.readlines()
+                        data.sort()
+                        print(data)
+                        # with open(fname_sorted, "w") as f1:
+                        #     for line in data:
+                        #         f.write(line)
+                        # f1.close()
+                    f.close()
 
 
 if __name__ == "__main__":
---
+++ demo/myfile.md
@@ -0,0 +1,41 @@
+
+**Gather VM info from OVM**
+---------------------------
+
+This script will connect to OVM and provide  following Information
+If you don't Specify the username - it will assume default user as admin and ask for password
+
+**Actions:**
+
+```    listvms     - It will list all VMs on the OVM with corresponding Server where it is hosted
+       listsrvs    - It will list all OVS servers
+       showvmd     - List of all disk attached to particular VM
+       srvdisks    - List disks attached to OVS server
+       adddisks    - It will take your answer file and  will create all commands which you need to run on OVM
+                       - adddisks action will do following checks
+                       - It will validate your answerfile by checking following
+                               -  Check any duplication slot numbers in your file
+                               -  Check any duplication on provided disks
+                               -  Check any duplicates in the disk names
+                               -  It will validate all slot numbers - whether it is used already on the VM
+                               -  Will check if the disknames are already used on the VM
+                               -  Will check if the physical disks are already attached to VM
+```
+
+**Answer File Specification:**
+
+        * It should be comma sperated
+        * one disk info per one line
+        * Header Needs to be added
+
+**Sample  answer file:**
+
+```
+        slot,disk,sharble,diskname
+        12,6005076810810103F000000000000056,NO,a0680o3odbsd415IBM-OS
+        13,6005076810810103F000000000000057,nO,a0680o3odbsd415IBM-swap
+        14,6005076810810103F00000000000006D,YES,+ocr_vote1-a0680o3odbsd415_416_417
+```
+
+*** Sample Commnds: ***
+
